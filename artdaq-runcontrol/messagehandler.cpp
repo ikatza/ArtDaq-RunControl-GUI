60e6b5d001c6700fe972658f4396c583fb8b955d
Iker Loïc de Icaza Astiz <icaza@fnal.gov>
2020-01-29 09:59:23 -0600

    Improve formatting of output messages

--- artdaq-runcontrol/messagehandler.cpp
+++ artdaq-runcontrol/messagehandler.cpp
@@ -28,8 +28,8 @@ void messageOutput(QtMsgType type, const QMessageLogContext &context, const QStr
 
 void customLogWriter(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
   QByteArray localMsg = msg.toLocal8Bit();
-  const char *file = context.file ? context.file : "---";
-  const char *function = context.function ? context.function : "***";
+  const char *file = context.file ? context.file : "";
+  const char *function = context.function ? context.function : "";
   switch (type) {
   case QtDebugMsg:
     LOG_WRITE(std::cout, COLOR_DEBUG, "DEBUG", localMsg.constData(), file, context.line, function);
@@ -38,10 +38,10 @@ void customLogWriter(QtMsgType type, const QMessageLogContext &context, const QS
     LOG_WRITE(std::cout, COLOR_INFO, "INFO", localMsg.constData(), file, context.line, function);
     break;
   case QtWarningMsg:
-    LOG_WRITE(std::cout, COLOR_WARN, "WARN", localMsg.constData(), file, context.line, function);
+    LOG_WRITE(std::cout, COLOR_WARN, "WARNING", localMsg.constData(), file, context.line, function);
     break;
   case QtCriticalMsg:
-    LOG_WRITE(std::cout, COLOR_CRITICAL, "CRIT", localMsg.constData(), file, context.line, function);
+    LOG_WRITE(std::cout, COLOR_CRITICAL, "CRITICAL", localMsg.constData(), file, context.line, function);
     break;
   case QtFatalMsg:
     LOG_WRITE(std::cout, COLOR_FATAL, "FATAL", localMsg.constData(), file, context.line, function);
--- artdaq-runcontrol/messagehandler.h
+++ artdaq-runcontrol/messagehandler.h
@@ -7,14 +7,14 @@
 #include <QTime>
 #include <iostream>
 
-#define COLOR_DEBUG "\033[32;1m"
-#define COLOR_INFO "\033[32;1m"
-#define COLOR_WARN "\033[33;1m"
-#define COLOR_CRITICAL "\033[31;1m"
-#define COLOR_FATAL "\033[33;1m"
+#define COLOR_DEBUG "\033[1;37m"
+#define COLOR_INFO "\033[1;32m"
+#define COLOR_WARN "\033[1;33m"
+#define COLOR_CRITICAL "\033[1;31m"
+#define COLOR_FATAL "\033[41m"
 #define COLOR_RESET "\033[0m"
 
-#define LOG_WRITE(OUTPUT, COLOR, LEVEL, MSG, FILE, LINE, FUNCTION) OUTPUT << COLOR << QTime::currentTime().toString("hhmmsszzz").toStdString() << " " LEVEL " " << COLOR_RESET << MSG << FILE << LINE << FUNCTION << "\n"
+#define LOG_WRITE(OUTPUT, COLOR, LEVEL, MSG, FILE, LINE, FUNCTION) OUTPUT << COLOR << QTime::currentTime().toString("hhmmsszzz").toStdString() << " " LEVEL " " << COLOR_RESET << " FILE::" << FILE << " LINE::" << LINE << " FUNCTION::" << FUNCTION << "\n\t" << COLOR << MSG << COLOR_RESET << "\n";
 
 void messageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
